61b454bb521c38e290e5128e163e3b6c34386a90
Eden Syoum <eden@temple.edu>
2016-08-02 21:04:54 -0400

    updated anchor names

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#ccddff', 'whitesmoke'],
-    anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+    anchors: ['one', 'two', 'three', 'four', 'five'],
     menu: '#menu',
     continuousVertical: true,
     responsiveHeight: 600
