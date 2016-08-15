51f3fc7251ab8166500e7aaceb278f40727e557e
Eden Syoum <eden@temple.edu>
2016-08-15 11:28:40 -0400

    making changes to styling and order of pages

--- js/app.js
+++ js/app.js
@@ -2,12 +2,22 @@
  * fullPage 2.8.2 - Menu
  * https://github.com/alvarotrigo/fullPage.js
  */
+
 $(document).ready(function() {
   $('#fullpage').fullpage({
     sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#ccddff', 'whitesmoke'],
     anchors: ['one', 'two', 'three', 'four', 'five'],
     menu: '#menu',
     continuousVertical: true,
-    responsiveHeight: 600
+    responsiveHeight: 600,
+  	slidesNavigation: true,
+    scrollOverflow: true,
+    scrollOverflowOptions: {
+      scrollbars: true,
+      mouseWheel: true,
+      hideScrollbars: false,
+      fadeScrollbars: false,
+      disableMouse: true
+    }
   });
 });
